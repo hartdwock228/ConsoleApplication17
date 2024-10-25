f6c3ca39e806af216837158521437030f0e6142c
Егор <@DESKTOP-VF4U8Q2>
2024-10-25 18:55:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication17/ConsoleApplication17.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int width, height;
+    cout << "Введите ширину и высоту рамки: ";
+    cin >> width >> height;
+
+    for (int i = 0; i < width; ++i) {
+        cout << "-";
+    }
+    cout << endl;
+
+    for (int i = 0; i < height - 2; ++i) {
+        cout << "|";
+        for (int j = 0; j < width - 2; ++j) {
+            cout << " ";
+        }
+        cout << "|" << endl;
+    }
+    if (height > 1) {
+        for (int i = 0; i < width; ++i) {
+            cout << "-";
+        }
+        cout << endl;
+    }
+
+
+}
